df753070d2cc0d5c99296f5747dbf2c7fe4fd245
angel.vazquez <anjo.vazquez@gmail.com>
2018-08-09 15:02:41 +0200

    feat. Replaced gmaps with mapbox leaflet

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,14 @@
 let restaurants,
   neighborhoods,
-  cuisines,
-  map
+  cuisines
+var newMap
 var markers = []
 
 /**
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
  */
 document.addEventListener('DOMContentLoaded', (event) => {
+  initMap();
   fetchNeighborhoods();
   fetchCuisines();
 });
@@ -69,10 +70,27 @@ fillCuisinesHTML = (cuisines = self.cuisines) => {
   });
 }
 
+initMap = () => {
+  self.newMap = L.map('map', {
+        center: [40.722216, -73.987501],
+        zoom: 12,
+        scrollWheelZoom: false
+      });
+  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
+    mapboxToken: 'pk.eyJ1IjoiYW5qb3ZhenF1ZXoiLCJhIjoiY2o2dHcza2Y0MHduYTJxcXN6NHAzbDZpZCJ9.jj-yFqjE7Ru3YVtIcdcgXw',
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+      '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+      'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'mapbox.streets'
+  }).addTo(newMap);
+  updateRestaurants();
+}
+
 /**
  * Initialize Google map, called from HTML.
  */
-window.initMap = () => {
+/*window.initMap = () => {
   let loc = {
     lat: 40.722216,
     lng: -73.987501
@@ -83,7 +101,7 @@ window.initMap = () => {
     scrollwheel: false
   });
   updateRestaurants();
-}
+}*/
 
 /**
  * Update page and map for current restaurants.
@@ -201,10 +219,21 @@ createRestaurantHTML = (restaurant) => {
   return li;
 }
 
+addMarkersToMap = (restaurants = self.restaurants) => {
+  restaurants.forEach(restaurant => {
+    // Add marker to the map
+    const marker = DBHelper.mapMarkerForRestaurant(restaurant, self.newMap);
+    marker.on("click", onClick);
+    function onClick() {
+      window.location.href = marker.options.url;
+    }
+  });
+} 
+
 /**
  * Add markers for current restaurants to the map.
  */
-addMarkersToMap = (restaurants = self.restaurants) => {
+/*addMarkersToMap = (restaurants = self.restaurants) => {
   restaurants.forEach(restaurant => {
     // Add marker to the map
     const marker = DBHelper.mapMarkerForRestaurant(restaurant, self.map);
@@ -213,4 +242,4 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     });
     self.markers.push(marker);
   });
-}
+}*/
--- src/js/restaurant_info.js
+++ src/js/restaurant_info.js
@@ -1,10 +1,41 @@
 let restaurant;
-var map;
+var newMap;
+
+document.addEventListener('DOMContentLoaded', (event) => {  
+  initMap();
+});
+
+/**
+ * Initialize leaflet map
+ */
+initMap = () => {
+  fetchRestaurantFromURL((error, restaurant) => {
+    if (error) { // Got an error!
+      console.error(error);
+    } else {      
+      self.newMap = L.map('map', {
+        center: [restaurant.latlng.lat, restaurant.latlng.lng],
+        zoom: 16,
+        scrollWheelZoom: false
+      });
+      L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
+        mapboxToken: 'pk.eyJ1IjoiYW5qb3ZhenF1ZXoiLCJhIjoiY2o2dHcza2Y0MHduYTJxcXN6NHAzbDZpZCJ9.jj-yFqjE7Ru3YVtIcdcgXw',
+        maxZoom: 18,
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+          '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+          'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        id: 'mapbox.streets'    
+      }).addTo(newMap);
+      fillBreadcrumb();
+      DBHelper.mapMarkerForRestaurant(self.restaurant, self.newMap);
+    }
+  });
+}
 
 /**
  * Initialize Google map, called from HTML.
  */
-window.initMap = () => {
+/*window.initMap = () => {
   fetchRestaurantFromURL((error, restaurant) => {
     if (error) { // Got an error!
       console.error(error);
@@ -18,7 +49,7 @@ window.initMap = () => {
       DBHelper.mapMarkerForRestaurant(self.restaurant, self.map);
     }
   });
-}
+}*/
 
 /**
  * Get current restaurant from page URL.
